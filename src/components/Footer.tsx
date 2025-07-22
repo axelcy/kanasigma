a66c61be0b10ae71fa42af7e75b5849983100a8d
Axel Cymerman <axelcy@gmail.com>
2025-07-21 21:43:21 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import '@styles/Footer.css'
 function Footer() {
     return (
         <footer>
-            <span>Made with ❤️ - <a href="https://github.com/katasigma">KanaSigma</a></span>
+            <span>Made with ❤️ - <a href="https://github.com/axelcy/kanasigma" target='_blank'>KanaSigma</a></span>
         </footer>
     )
 }
